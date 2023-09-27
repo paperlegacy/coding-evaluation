d3af624dc583cc939ac23f209134e8743ab1489b
Christian Butler <paperlegacy@gmail.com>
2023-09-27 12:22:49 -0700

    Removed unnecessary usings in the Organization.cs class, created recursive method to search for the existing position and assign the new employee when the position has been found and vacant, updated the hire method to make the project functional.

--- dotnet/MyOrganization/Organization.cs
+++ dotnet/MyOrganization/Organization.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Immutable;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MyOrganization
 {
@@ -26,7 +23,58 @@ namespace MyOrganization
          */
         public Position? Hire(Name person, string title)
         {
-            //your code here
+            bool isRootPosition = root.GetTitle() == title;            
+            Random ranNum = new();
+
+            /*
+             * using random number with a max range of 10000 to minimize the risk of duplicate Employee Ids for this example...
+             * NOT what should be used for a real-world scenario. The Employee constructor requires an int (identifier) when 
+             * creating an instance of the object (Employee).
+            */
+            var newHire = new Employee(ranNum.Next(10000), person);          
+            
+            // Check the root (Position) to see if the title matches.
+            if (isRootPosition && !root.IsFilled())
+            {
+                root.SetEmployee(newHire);
+            }
+
+            return isRootPosition ? root : FindPositionAndAssignEmployee(root.GetDirectReports(), newHire, title);
+        }
+
+        /// <summary>
+        /// Recursive method to find the correct position, assign the new hire if the position has not been filled, 
+        /// or return filled position or null if the position does not exist.
+        /// </summary>
+        /// <param name="posList"></param>
+        /// <param name="newHire"></param>
+        /// <param name="title"></param>
+        /// <returns>Returns position or null if the position does not exist.</returns>
+        private Position? FindPositionAndAssignEmployee(ImmutableList<Position> posList, Employee newHire, string title)
+        {
+            if (posList != null)
+            {
+                foreach (Position p in posList)
+                {
+                    // Position found and has already been filled.
+                    if (p.GetTitle() == title && p.IsFilled())
+                    {
+                        return p;
+                    }
+                    // Position found and has not been filled, assign the new employee.
+                    else if (p.GetTitle() == title && !p.IsFilled())
+                    {
+                        p.SetEmployee(newHire);
+                        return p;
+                    }
+                    // Continue to dig for the position using recursion.
+                    else
+                    {
+                        FindPositionAndAssignEmployee(p.GetDirectReports(), newHire, title);
+                    }
+                }
+            }
+            // Positon has not been found after going through all avaialble positions, return null.
             return null;
         }
 
